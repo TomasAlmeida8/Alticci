51f6ba35a89df28e8c9aeb4f232b0d36669ee270
Tomas <tomasalmeida20028@gmail.com>
2023-09-11 18:26:21 +0100

    dockerized

--- backend/src/test/java/com/alticci/CalculatorResourceTest.java
+++ backend/src/test/java/com/alticci/CalculatorResourceTest.java
@@ -2,7 +2,6 @@ package com.alticci;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -12,10 +11,10 @@ public class CalculatorResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/calculator")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .pathParam("n", 123) // Replace 123 with the desired value for 'n'
+        .when()
+            .get("/alticci/{n}")
+        .then()
+            .statusCode(200);
     }
-
 }
