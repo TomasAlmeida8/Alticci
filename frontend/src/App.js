f8eba1aa8440d3f0a1126078681259a4b777202e
Tomas <tomasalmeida20028@gmail.com>
2023-09-12 17:12:08 +0100

    quality of life improvement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,53 @@
-// App.js
 import React, { useState } from 'react';
 import './App.css';
 import NumberInput from './NumberInput';
+import Decimal from 'decimal.js';
 
-// Function to format a number with line breaks
-function formatNumber(number) {
-  const numberStr = number.toString();
-  const chunks = [];
-  const chunkSize = 65; // You can adjust this value to fit your layout
+function formatNumber(numberStr, decimalPlaces = 2) {
+  const number = new Decimal(numberStr);
 
-  for (let i = 0; i < numberStr.length; i += chunkSize) {
-    chunks.push(numberStr.substring(i, i + chunkSize));
+  if (number.isNaN()) {
+    return 'Invalid Number';
   }
 
-  return chunks.join('\n');
+  if (number.abs().lessThanOrEqualTo(1e9) && number.abs().greaterThan(1e-9)) {
+    return number.toString();
+  } else {
+    return number.toExponential(decimalPlaces);
+  }
 }
 
 function App() {
-  // State for the calculated result
   const [result, setResult] = useState('');
+  const [showWholeNumber, setShowWholeNumber] = useState(false); // State to control whether to show the whole number
+  const [showScientific, setShowScientific] = useState(true); // State to control whether to show scientific notation
 
-  // Function to handle the calculation result
   const handleCalculate = (calculatedResult) => {
     setResult(calculatedResult);
-    console.log('Result in App.js:', calculatedResult); // Log the result
+    setShowWholeNumber(false); // Reset the showWholeNumber state when a new result is calculated
+    setShowScientific(true); // Ensure scientific notation is initially turned on
+    console.log('Result in App.js:', calculatedResult);
+  };
+
+  // Function to toggle the showWholeNumber state
+  const toggleShowWholeNumber = () => {
+    setShowWholeNumber(!showWholeNumber);
+    setShowScientific(false); // Ensure scientific notation is turned off
+  };
+
+  // Function to toggle the showScientific state
+  const toggleShowScientific = () => {
+    setShowScientific(!showScientific);
+    setShowWholeNumber(false); // Ensure whole number is turned off
+  };
+
+  const renderWholeNumber = (number) => {
+    const chunkSize = 65;
+    const chunks = [];
+    for (let i = 0; i < number.length; i += chunkSize) {
+      chunks.push(number.slice(i, i + chunkSize));
+    }
+    return chunks.map((chunk, index) => <pre key={index}>{chunk}</pre>);
   };
 
   return (
@@ -36,7 +60,29 @@ function App() {
         {result !== '' && (
           <div className="Result">
             <h2>Result:</h2>
-            <pre>{formatNumber(result)}</pre>
+            {showWholeNumber ? (
+              renderWholeNumber(result)
+            ) : (
+              <>
+                {showScientific ? (
+                  <>
+                    <pre>{formatNumber(result)}</pre>
+                    <button onClick={toggleShowWholeNumber}>
+                      Show Whole Number
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <pre>{result}</pre>
+                    {result.length >= 10 && (
+                      <button onClick={toggleShowScientific}>
+                        Show Scientific Notation
+                      </button>
+                    )}
+                  </>
+                )}
+              </>
+            )}
           </div>
         )}
       </main>
